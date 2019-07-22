865f86605db40019493070439812ca9552a0823b
m888ii888m <mengke8292963@163.com>
2019-07-22 11:30:40 +0800

    #12413

--- multi3/src/main/java/artifactory/test/Multi3.java
+++ multi3/src/main/java/artifactory/test/Multi3.java
@@ -9,5 +9,6 @@ public class Multi3 {
         System.out.println("Hello World!");
         System.out.println("你好!");
         System.out.println("你好!");
+        System.out.println("你好!");
     }
 }
