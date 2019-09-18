3f68d1a9c45b8a7f76782401f40a4a5dcf3d840f
mengke <mengke8292963@163.com>
2019-09-18 16:10:00 +0800

    #20190918

--- multi3/src/main/java/artifactory/test/Multi3.java
+++ multi3/src/main/java/artifactory/test/Multi3.java
@@ -13,8 +13,6 @@ public class Multi3 {
         System.out.println("你好!");
         System.out.println("你好!");
         System.out.println("HelloDemo");
-        System.out.println("HelloDemo20190829");
-        System.out.println("HelloDemo20190830");
         System.out.println("HelloDemo20190918");
     }
 }
