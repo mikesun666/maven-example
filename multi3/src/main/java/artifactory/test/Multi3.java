98666f5ed3452c2b10dfc8854262b2192abf673b
mengke <mengke8292963@163.com>
2019-11-13 16:59:55 +0800

    #20191113

--- multi3/src/main/java/artifactory/test/Multi3.java
+++ multi3/src/main/java/artifactory/test/Multi3.java
@@ -15,5 +15,6 @@ package artifactory.test;
             System.out.println("HelloDemo");
             System.out.println("HelloDemo20190919");
             System.out.println("HelloDemo20190919");
+            System.out.println("HelloDemo20190129");
         }
 }
