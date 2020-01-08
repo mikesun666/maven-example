f40176313347d9db9213d91356864078c43dcb6f
mengke <mengke8292963@163.com>
2020-01-08 10:43:34 +0800

    #20200108

--- multi3/src/main/java/artifactory/test/Multi3.java
+++ multi3/src/main/java/artifactory/test/Multi3.java
@@ -1,20 +1,19 @@
 package artifactory.test;
 
-/**
- * Hello world!
- */
-    public class Multi3 {
-        public static void main(String[] args) {
-            new Multi3();
-            System.out.println("Hello World·!");
-            System.out.println("你好!");
-            System.out.println("你好!");
-            System.out.println("你好!");
-            System.out.println("你好!");
-            System.out.println("你好!");
-            System.out.println("HelloDemo");
-            System.out.println("HelloDemo20190919");
-            System.out.println("HelloDemo20190919");
-            System.out.println("HelloDemo20190129");
-        }
+/*** Hello world!*/
+public class Multi3 {
+    public static void main(String[] args) {
+        new Multi3();
+        System.out.println("Hello World·!");
+        System.out.println("你好!");
+        System.out.println("你好!");
+        System.out.println("你好!");
+        System.out.println("你好!");
+        System.out.println("你好!");
+        System.out.println("HelloDemo");
+        System.out.println("HelloDemo20190919");
+        System.out.println("HelloDemo20190919");
+        System.out.println("HelloDemo20190129");
+        System.out.println("HelloDemo20200108");
+    }
 }
